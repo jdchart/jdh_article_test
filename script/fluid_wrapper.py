144f9fcdc886ca137e65f42e4826a16ce7bf00c9
Jacob Hart <jacob@uc12825.uni.lux>
2025-12-09 15:35:59 +0100

    fluid process test

--- script/fluid_wrapper.py
+++ script/fluid_wrapper.py
@@ -1,5 +1,8 @@
 import os
 import stat
+import uuid
+import scipy
+import subprocess
 
 def init_fluid():
     make_executable(os.path.join(os.getcwd(), "script", "flucoma-cli", "fluid-mfcc"))
@@ -11,3 +14,17 @@ def make_executable(file_path):
         print(f"Made '{file_path}' executable")
     else:
         print(f"File '{file_path}' does not exist")
+
+def create_temp():
+    os.makedirs(os.path.join(os.getcwd(), ".temp"), exist_ok = True)
+
+def wav_spill(file_path):
+    rate, data = scipy.io.wavfile.read(file_path)
+    return data
+
+def slicer_test(source):
+    create_temp()
+    result_file_name = os.path.join(os.getcwd(), ".temp", f"{str(uuid.uuid4()).wav}")
+    subprocess.run(["fluid-onsetslice", "-source", source, "-indices", result_file_name])
+    result = wav_spill(result_file_name)
+    return result
